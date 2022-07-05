1b29e0ea72b2711879413813cc23573ae13e9b3a
diegoargottez@gmail.com <diegoargottez@gmail.com>
2022-07-05 12:12:38 -0400

    finished country states city

--- ShoppingPrueba/ShoppingPrueba/Controllers/CountriesController.cs
+++ ShoppingPrueba/ShoppingPrueba/Controllers/CountriesController.cs
@@ -34,7 +34,7 @@ namespace ShoppingPrueba.Controllers
                 return NotFound();
             }
 
-            var country = await _context.countries.Include(c => c.States)
+            var country = await _context.countries.Include(c => c.States).ThenInclude(c=>c.Cities)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (country == null)
             {
@@ -42,6 +42,107 @@ namespace ShoppingPrueba.Controllers
             }
 
             return View(country);
+        }
+
+
+        // GET: Countries/Details/5
+        public async Task<IActionResult> DetailsState(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var state = await _context.States.Include(s => s.Cities).Include(s=>s.Country)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return View(state);
+        }
+        public async Task<IActionResult> DetailsCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            City city = await _context.Cities.Include(c => c.State)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            City city = await _context.Cities.Include(s => s.State).FirstOrDefaultAsync(s => s.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            CityViewModel model = new()
+            {
+               StateId=city.State.Id,
+               Id=city.Id,
+               Name=city.Name,
+
+            };
+            return View(model);
+        }
+
+        // POST: Countries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditCity(int id, CityViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    City city = new City()
+                    {
+                        Id=model.Id,
+                        Name = model.Name,
+                    };
+                    _context.Update(city);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(DetailsState), new { Id = model.StateId });
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una ciudad con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
         }
 
 
@@ -72,7 +173,7 @@ namespace ShoppingPrueba.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditState(int id,StateViewModel model)
+        public async Task<IActionResult> EditState(int id,  StateViewModel model)
         {
             if (id != model.Id)
             {
@@ -91,7 +192,7 @@ namespace ShoppingPrueba.Controllers
                     };
                     _context.Update(state);
                     await _context.SaveChangesAsync();
-                   // return RedirectToAction(nameof(Details), new { Id = model.CountryId });
+                  return RedirectToAction(nameof(Details), new { Id = model.CountryId });
                 }
                 catch (DbUpdateException dbUpdateException)
                 {
@@ -125,7 +226,7 @@ namespace ShoppingPrueba.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+        public async Task<IActionResult> Create(Country country)
         {
             if (ModelState.IsValid)
             {
@@ -176,7 +277,7 @@ namespace ShoppingPrueba.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,MyProperty")] Country country)
+        public async Task<IActionResult> Edit(int id, Country country)
         {
             if (id != country.Id)
             {
@@ -189,6 +290,7 @@ namespace ShoppingPrueba.Controllers
                 {
                     _context.Update(country);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateException dbUpdateException)
                 {
@@ -210,6 +312,7 @@ namespace ShoppingPrueba.Controllers
         }
 
         // GET: Countries/Delete/5
+        [HttpGet]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -228,17 +331,87 @@ namespace ShoppingPrueba.Controllers
         }
 
         // POST: Countries/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            var country = await _context.countries.Include(c=>c.States).FirstOrDefaultAsync(m => m.Id == id);
-
-            _context.States.RemoveRange(country.States);
-            _context.countries.Remove(country);
+            Country country = await _context.countries.Include(c => c.States).ThenInclude(c => c.Cities).FirstOrDefaultAsync(m => m.Id == id);
+                foreach (var item in country.States) {
+                    _context.Cities.RemoveRange(item.Cities);
+                };
+                foreach (var item in country.States) {
+                    _context.States.Remove(item);
+                };
+                _context.countries.Remove(country);
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> DeleteState(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            States state = await _context.States.Include(c => c.Country).Include(c=>c.Cities)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return View(state);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteState(int id)
+        {
+            States state = await _context.States.Include(c => c.Country).Include(c=>c.Cities).FirstOrDefaultAsync(m => m.Id == id);
+                //foreach (var item in state)
+                // {
+                //    _context.Cities.RemoveRange(item.Cities);
+                //};
+                foreach (var item in state.Cities)
+                {
+                    _context.Cities.Remove(item);
+                };
+                 _context.States.Remove(state);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Details),new { Id=state.Country.Id});
+        }
+        [HttpGet]
+        public async Task<IActionResult> DeleteCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            City city = await _context.Cities.Include(c => c.State)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            City city = await _context.Cities.Include(c => c.State).FirstOrDefaultAsync(m => m.Id == id);
+            _context.Cities.Remove(city);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(DetailsState), new {Id=city.State.Id });
         }
 
         [HttpGet]
@@ -285,6 +458,64 @@ namespace ShoppingPrueba.Controllers
                     if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
                         ModelState.AddModelError(string.Empty, "Ya existe un estado con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            States state = await _context.States.FindAsync(id);
+            if (state == null) return NotFound();
+
+            CityViewModel model = new()
+            {
+                StateId = state.Id,
+            };
+            return View(model);
+        }
+
+
+        // POST: Countries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddCity(CityViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    City city = new City()
+                    {
+                        State = await _context.States.FindAsync(model.StateId),
+                        Name = model.Name,
+                    };
+                    _context.Add(city);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(DetailsState), new { Id = model.StateId });
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una ciudad con el mismo nombre.");
                     }
                     else
                     {
---
+++ ShoppingPrueba/ShoppingPrueba/Models/CityViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingPrueba.Models
+{
+    public class CityViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Ciudad")]
+        [MaxLength(50, ErrorMessage = "Campo {0} debe tener máximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public String Name { get; set; }
+        public int StateId { get; set; }
+    }
+}
